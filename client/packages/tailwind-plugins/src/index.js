4a0fcdc9c2f80e94fd78d6f9082c0944de20c909
Chi Cuong Vuong <vuongcuong.phoenix@gmail.com>
2022-02-18 16:15:50 +0700

    feat(tailwind-plugins): add `focusibleComopnents`

--- client/packages/tailwind-plugins/src/index.js
+++ client/packages/tailwind-plugins/src/index.js
@@ -20,6 +20,35 @@ const plugins = {
 
     addUtilities(wh);
   },
+
+  focusibleComponents: ({ addComponents }) => {
+    const states = [
+      {
+        name: 'default',
+        state: 'focus',
+      },
+      {
+        name: 'within',
+        state: 'focus-within',
+      },
+    ];
+
+    const focusible = states.reduce(
+      (accum, { name, state }) => ({
+        ...accum,
+        [`.focusible-${name}`]: {
+          '@apply transition duration-300': {},
+
+          [`&:${state}`]: {
+            '@apply dark:border-primary-dark-darker ring ring-primary-dark-darker/40': {},
+          },
+        },
+      }),
+      {}
+    );
+
+    addComponents(focusible);
+  },
 };
 
 module.exports = plugin((helpers) => {
