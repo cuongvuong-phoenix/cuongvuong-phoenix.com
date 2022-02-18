4a3f4a10b556eba02317fb1c7df060cc6064fa5e
Chi Cuong Vuong <vuongcuong.phoenix@gmail.com>
2022-02-18 10:20:06 +0700

    feat(website/postss): add `postcss-calc` and `postcss-each` plugins

--- client/packages/website/postcss.config.js
+++ client/packages/website/postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
   plugins: {
     'postcss-at-rules-variables': {},
     'postcss-import': {},
+    'postcss-calc': {},
+    'postcss-each': {},
     'postcss-for': {},
     'tailwindcss/nesting': {},
     'tailwindcss': {},
