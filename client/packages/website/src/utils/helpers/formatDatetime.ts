35db98c3552f21f775b16a108de525c953189f46
Chi Cuong Vuong <vuongcuong.phoenix@gmail.com>
2022-02-19 14:18:56 +0700

    feat(client/website/utils/helpers): add `date-fns` package and `formatDateTime` helper function

---
+++ client/packages/website/src/utils/helpers/formatDatetime.ts
@@ -0,0 +1,13 @@
+import { type Locale, format } from 'date-fns';
+import { enUS } from 'date-fns/locale';
+import { type AppLocale } from '~/locales/utils';
+
+const localeObjectMap: Record<AppLocale, Locale> = {
+  'en-US': enUS,
+};
+
+export function formatDatetime(date: Date, locale: string, formatString = 'P') {
+  return format(date, formatString, {
+    locale: localeObjectMap[locale as AppLocale],
+  });
+}
