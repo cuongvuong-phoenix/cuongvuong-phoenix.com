2f0d1f5768e8dd581e466c9e9a7da3d9a0334482
Chi Cuong Vuong <vuongcuong.phoenix@gmail.com>
2022-02-27 01:50:14 +0700

    feat(client/website/pages): add GraphQL query call to `blog/:post` page

--- client/packages/website/src/pages/BlogPost.vue
+++ client/packages/website/src/pages/BlogPost.vue
@@ -2,27 +2,27 @@
   <div>
     <!-- "Top" -->
     <!-- "Row 1 - Tags" -->
-    <div v-if="post.tags.length > 0" class="flex flex-wrap items-center justify-center space-x-2">
-      <UPill v-for="tag in post.tags" :key="tag.id" :icon="tag.icon" :name="tag.name" dim />
+    <div v-if="gqlPost && gqlPost.tags.length > 0" class="flex flex-wrap items-center justify-center space-x-2">
+      <UPill v-for="tag in gqlPost?.tags" :key="tag.id" :icon="tag.icon" :name="tag.name" dim />
     </div>
     <!-- END "Row 1 - Tags" -->
 
     <!-- "Row 2 - Title" -->
-    <h1 class="mt-6 font-serif text-4xl font-bold text-center">{{ post.title }}</h1>
+    <h1 class="mt-6 font-serif text-4xl font-bold text-center">{{ gqlPost?.title }}</h1>
     <!-- END "Row 2 - Title" -->
 
     <!-- "Row 3" -->
     <div class="flex items-center justify-center mt-6 space-x-4 text-fg-darker">
       <div class="flex items-center space-x-2">
         <UIcon icon="fluent:calendar-ltr-24-regular" />
-        <span>{{ formatDatetime(post.createdAt, locale) }}</span>
+        <span v-if="gqlPost">{{ formatDatetime(gqlPost.createdAt, locale) }}</span>
       </div>
 
-      <template v-if="post.updatedAt">
+      <template v-if="gqlPost?.updatedAt">
         <span class="dot-1"></span>
         <div class="flex items-center space-x-2">
           <UIcon icon="fluent:calendar-edit-24-regular" />
-          <span>{{ formatDatetime(post.updatedAt, locale) }}</span>
+          <span>{{ formatDatetime(gqlPost?.updatedAt, locale) }}</span>
         </div>
       </template>
 
@@ -30,50 +30,82 @@
       <div class="flex items-center space-x-2">
         <UIcon icon="fluent:book-clock-24-regular" />
         <span
-          >{{ post.readingTime }}
-          {{ `${t('common.minute', post.readingTime)} ${t('common.read')}`.toLowerCase() }}</span
+          >{{ gqlPost?.readingTime }}
+          {{ `${t('common.minute', gqlPost ? gqlPost.readingTime : 0)} ${t('common.read')}`.toLowerCase() }}</span
         >
       </div>
     </div>
     <!-- END "Row 3" -->
     <!-- END "Top" -->
 
-    <!-- "Body" -->
+    <!-- "Content" -->
     <div class="mx-auto mt-16 prose prose-light">
-      {{ post.body }}
+      {{ gqlPost?.content }}
     </div>
-    <!-- END "Body" -->
+    <!-- END "Content" -->
   </div>
 </template>
 
 <script setup lang="ts">
-  import { computed } from 'vue';
+  import { reactive, watch } from 'vue';
   import { useRoute } from 'vue-router';
   import { useI18n } from 'vue-i18n';
-  import faker from '@faker-js/faker/locale/en';
+  import { useQuery, useResult } from '@vue/apollo-composable';
+  import { gql } from 'graphql-tag';
+  import { parseISO } from 'date-fns';
   import { UIcon, UPill } from '@cvp-web-client/ui';
-  import { postTags } from '~/utils/mocks';
   import { formatDatetime } from '~/utils/helpers';
+  import type { PostQuery, PostQueryVariables } from '~/types/graphql';
 
   const route = useRoute();
   const { t, locale } = useI18n();
 
-  const slug = computed(() => route.params.post as string);
-
   /* ----------------------------------------------------------------
   READ post
   ---------------------------------------------------------------- */
-  const post = computed(
-    () =>
-      ({
-        id: faker.datatype.uuid(),
-        slug: slug.value,
-        title: faker.lorem.words(8),
-        createdAt: faker.date.past(1),
-        updatedAt: faker.random.arrayElement([faker.date.past(0.5), undefined]),
-        readingTime: faker.datatype.number(60),
-        tags: faker.random.arrayElements(postTags, faker.datatype.number(4)),
-        body: faker.lorem.paragraphs(16),
-      } as Model.PostR)
+  // Vairables.
+  const postQueryVariables = reactive({}) as PostQueryVariables;
+
+  watch(
+    () => route.params.post,
+    (postParam) => {
+      if (postParam) {
+        postQueryVariables.slug = postParam as string;
+      }
+    },
+    { immediate: true }
   );
+
+  // GraphQL.
+  const {
+    result: postResult,
+    loading: postLoading,
+    error: postError,
+  } = useQuery<PostQuery, PostQueryVariables>(
+    gql`
+      query post($slug: String!) {
+        post(slug: $slug) {
+          id
+          title
+          slug
+          readingTime
+          content
+          createdAt
+          updatedAt
+          tags {
+            id
+            name
+            icon
+          }
+        }
+      }
+    `,
+    postQueryVariables
+  );
+
+  const gqlPost = useResult(postResult, undefined, (data) => ({
+    ...data.post,
+    createdAt: parseISO(data.post.createdAt),
+    updatedAt: data.post.updatedAt ? parseISO(data.post.updatedAt) : undefined,
+  }));
 </script>
