70a1d62d838ba3c7f316d75ff9506c92fc570209
Chi Cuong Vuong <vuongcuong.phoenix@gmail.com>
2022-03-05 12:36:05 +0700

    feat, fix(client/website): add `vite-plugin-md` and useful MarkdownIt plugins & adjust styles & fix scroll behavior for `to.hash`

--- client/packages/website/src/router/index.ts
+++ client/packages/website/src/router/index.ts
@@ -1,25 +1,34 @@
 import { promiseTimeout } from '@vueuse/core';
 import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from './routes';
+import { HeaderHeight } from '~/store/ui';
 
 export * from './routes';
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior: async (to, _, savedPosition) => {
+  scrollBehavior: async (to, from, savedPosition) => {
     const behavior: ScrollBehavior = 'smooth';
 
-    if (to.meta.transition && to.meta.transitionLeaveDuration) {
-      await promiseTimeout(to.meta.transitionLeaveDuration);
+    if (!to.hash) {
+      if (to.meta.transition && to.meta.transitionLeaveDuration) {
+        await promiseTimeout(to.meta.transitionLeaveDuration);
+      } else {
+        await promiseTimeout(250);
+      }
     } else {
-      await promiseTimeout(250);
+      await promiseTimeout(0);
     }
 
-    if (savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: HeaderHeight.NARROW + 8,
+        behavior: !from.name ? behavior : undefined,
+      };
+    } else if (savedPosition) {
       return { ...savedPosition, behavior };
-    } else if (to.hash) {
-      return { el: to.hash, behavior };
     } else {
       return { top: 0, behavior };
     }
