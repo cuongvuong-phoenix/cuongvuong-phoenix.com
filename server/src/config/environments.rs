5e819574f44f679b318397341b2d49c4e67bfbc3
Cuong Vuong Chi <cuongvuong.phoenix@gmail.com>
2022-03-14 17:13:46 +0700

    refactor(server): use separate `HOST` and `PORT` env vars to be compatible with Heroku

--- server/src/config/environments.rs
+++ server/src/config/environments.rs
@@ -1,9 +1,16 @@
 use once_cell::sync::Lazy;
 use std::env;
 
-pub static ADDRESS: Lazy<String> = Lazy::new(|| env::var("ADDRESS").expect("ADDRESS is set"));
+pub static ADDRESS: Lazy<String> = Lazy::new(|| {
+    let host = env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port = env::var("PORT").expect("PORT is set");
+
+    format!("{}:{}", host, port)
+});
+
 pub static ALLOWED_ORIGIN: Lazy<Vec<String>> =
     Lazy::new(|| get_list_env(env::var("ALLOWED_ORIGIN").expect("ALLOWED_ORIGIN is set")));
+
 pub static DATABASE_URL: Lazy<String> =
     Lazy::new(|| env::var("DATABASE_URL").expect("DATABASE_URL is set"));
 
