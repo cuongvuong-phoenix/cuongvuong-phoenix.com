aeec72e80a45426daf4147e803e09d9621053521
Cuong Vuong Chi <cuongvuong.phoenix@gmail.com>
2022-08-18 23:01:14 +0700

    fix(client/website/components): add `scope="global"` for `<i18n-t>` component to fix warning

--- client/packages/website/src/components/WFooter.vue
+++ client/packages/website/src/components/WFooter.vue
@@ -2,7 +2,7 @@
   <footer class="py-12">
     <div class="container mx-auto text-center">
       <!-- "Left" -->
-      <i18n-t keypath="footer.about" tag="p" class="text-fg-darker">
+      <i18n-t keypath="footer.about" tag="p" class="text-fg-darker" scope="global">
         <span class="text-fg-default">{{ t('common.whoami') }}</span>
         <br />
         <span class="text-fg-default">Vue, GraphQL, Rust, PostgreSQL</span>
@@ -14,7 +14,6 @@
 
 <script setup lang="ts">
   import { useI18n } from 'vue-i18n';
-  import { UButton, UIcon } from '@cuongvuong-phoenix-com-client/ui';
 
   const { t } = useI18n();
 </script>
