7dac4852b818800f531a37f986ee6c5c2e2116f6
Cuong Vuong <cuongvuong.phoenix@gmail.com>
2022-09-07 00:32:19 +0700

    fix(server/graphql/shared/errors): explicitly add an event at ERROR level

--- server/src/graphql/shared/errors.rs
+++ server/src/graphql/shared/errors.rs
@@ -10,7 +10,7 @@ impl ErrorExtensions for SharedError {
     fn extend(&self) -> async_graphql::Error {
         Error::new(self.to_string()).extend_with(|_, v| match self {
             SharedError::Database(error) => {
-                tracing::debug!("{}", error);
+                tracing::error!("Database Error: {}", error);
                 v.set("code", "500")
             }
         })
